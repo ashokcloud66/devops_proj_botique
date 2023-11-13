4ce5e325baa80555c4d1816841c1a797b7329a8f
pavankumar1112 <pkkorlakunta@gmail.com>
2023-11-13 11:54:56 +0530

    public ip

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -22,8 +22,8 @@ module "eks" {
     #count = length(module.vpc.private_subnets_id)
     }
 
-output "node_instance_pub_ip" {
-    value = module.eks.eks_node_instance_public_ip 
+output "eks_node_instance_public_ip" {
+    value = module.eks.aws_instance.eks_node_instance.public_ip
 }
 output "kubernetes-endpoint" {
     value = module.eks.eks_cluster_endpoint
